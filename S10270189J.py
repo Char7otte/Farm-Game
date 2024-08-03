e6b1cd14f5c7d22850f872cc56762a7b090be868
shar <faethychan@gmail.com>
2024-08-03 18:19:43 +0800

    Update farm_layout to include remaining growth time

--- S10270189J.py
+++ S10270189J.py
@@ -12,7 +12,7 @@ player_variables = {
 farm_layout = [ 
     [None, None, None, None, None],
     [None, None, None, None, None],
-    [None, None, 'HSE', None, None],
+    [None, None, ["HSE", None], None, None],
     [None, None, None, None, None],
     [None, None, None, None, None] ]
 
